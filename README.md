1d495c845c244da31b4150f8d5faba05434e0d45
Nathan Nosar <nathan.nosar@gmail.com>
2021-02-22 13:45:51 -0800

    have to turn something in

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homework2
+
+I wanted to create a page that was modern, clean, and represented myself. It is still a work in progress getting the tabs to work and my bottom links to both be activated and link to my pages and contact info. However, I need to get something turned in now... so here it goes. 
---
+++ script.js
@@ -0,0 +1,35 @@
+/* function setupTabs () {
+    document.querySelectorAll(".tabsButton").forEach(button => {
+        button.addEventListener('click', () =>{
+            const sideBar = button.parentElement;
+            const tabsContainer = sideBar.parentElement;
+            const tabsNumber = a.dataset.forTab;
+            const toActivate = tabsContainer.querySelector(`.tabsContent[data-tab="${tabNumber}"]`);
+            /* 
+            sideBar.querySelectorAll('.tabsButton').forEach(button => {
+                button.classList.remove('tabsButtonActive');
+            });
+
+            tabsContainer.querySelectorAll('.tabsContent').forEach( tab=> {
+                tab.classList.remove('tabsContentActive');
+            });
+
+            button.classList.add('tabsButtonActive');
+            tabToActivate.classList.add('tabsContentActive'); */
+/* 
+            console.log(sideBar);
+            console.log(tabsContainer);
+            console.log(tabsNumber);
+            console.log(toActivate);
+
+        });
+    });
+}
+
+        document.querySelectorAll('.tabs').forEach(tabsContainer => {
+            tabsContainer.querySelector('.tabsSideBar').click();
+        });
+
+
+    setupTabs(); */ 
+
